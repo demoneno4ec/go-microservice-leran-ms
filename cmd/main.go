023787edba2fc8f8fcaea84957f0d279ee3cd68e
m.pestov <m.pestov@12storeez.com>
2023-06-14 14:20:37 +0300

    Добавил использование бд

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"github.com/demoneno4ec/go-microservice-leran-ms/database"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	database.ConnectDb()
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
---
+++ database/database.go
@@ -0,0 +1,51 @@
+package database
+
+import (
+	"fmt"
+	"github.com/demoneno4ec/go-microservice-leran-ms/models"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type Dbinstance struct {
+	Db *gorm.DB
+}
+
+var DB Dbinstance
+
+func ConnectDb() {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai",
+		os.Getenv("DATABASE_HOST"),
+		os.Getenv("DATABASE_USER"),
+		os.Getenv("DATABASE_PASSWORD"),
+		os.Getenv("DATABASE_NAME"),
+		os.Getenv("DATABASE_PORT"),
+	)
+
+	options := &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	}
+	db, err := gorm.Open(postgres.Open(dsn), options)
+
+	if err != nil {
+		log.Fatal("Failed to connect to database. \n", err)
+		os.Exit(2)
+	}
+
+	log.Println("connected")
+	db.Logger = logger.Default.LogMode(logger.Info)
+
+	// TODO Спорно, это вроде миграции а не коннект к базе, т.е. миграции должны катиться, до инициализации конекта с бд.
+	// TODO Надо будет посмотреть на это пробрасывание зависимостей
+	log.Println("running migrations")
+	db.AutoMigrate(&models.Fact{})
+
+	DB = Dbinstance{
+		Db: db,
+	}
+}
